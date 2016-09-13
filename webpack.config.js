3911c066997848ef1fecd901a13522a84378c345
mashaal <omar@teacups.io>
2016-09-14 07:00:58 +1000

    webpack cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const cssnext = require('postcss-cssnext')
-const reporter = require('postcss-reporter')
 
 const config = {
   entry: [
@@ -30,47 +28,11 @@ const config = {
     extensions: ['', '.js', '.jsx'],
     root: path.join(__dirname, 'src')
   },
-  postcss: [
-    cssnext,
-    reporter({clearMessages: true})
-  ],
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false
     })
   ]
-}
-
-if (process.env.NODE_ENV === 'production') {
-  const ExtractTextPlugin = require('extract-text-webpack-plugin')
-  config.module.loaders.push({
-    test: /\.(css|postcss)$/,
-    loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer&modules!postcss')
-  })
-  config.plugins.push(
-    new ExtractTextPlugin('destroyer.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  )
-} else {
-  // config.entry.push('webpack-hot-middleware/client')
-  config.module.loaders.push({
-    test: /\.(css|postcss)$/,
-    loader: 'style!css?-autoprefixer&modules!postcss'
-  })
-  config.plugins.push(
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  )
 }
 
 module.exports = config
