1d7e249deb3378ca3eca3ad0922c914749cf192a
mashaal <omar@teacups.io>
2016-09-14 05:35:12 +1000

    add album animations
    Signed-off-by: mashaal <omar@teacups.io>

--- components/library/album.jsx
+++ components/library/album.jsx
@@ -2,14 +2,32 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Radium from 'radium'
 import { store } from '../../client.js'
+import inView from 'in-view'
 
 @Radium
 export default class Album extends Component {
   constructor (props) {
     super()
     this.state = {
-      active: false
+      active: false,
+      cover: '',
+      fade: true
     }
+  }
+  componentDidMount () {
+    this._mounted = true
+    this.album = ReactDOM.findDOMNode(this.refs.album)
+    this.container = ReactDOM.findDOMNode(this.props.container)
+    this.container.addEventListener('scroll', this.coverHandler)
+    this.coverHandler()
+  }
+  componentWillUnmount () {
+    this._mounted = false
+    this.container.removeEventListener('scroll', this.coverHandler)
+  }
+  coverHandler = () => {
+    if (inView.is(this.album)) this.setState({cover: this.props.album.cover, fade: false})
+    else this.setState({cover: '', fade: true})
   }
   handleClick = () => {
     store.dispatch({type: 'SHOWCASE', album: this.props.album})
@@ -22,10 +40,12 @@ export default class Album extends Component {
   }
   render () {
     let cover = ''
-    if (this.props.album.cover) cover = {backgroundImage: 'url(' + encodeURI(this.props.album.cover) + ')'}
+    if (this.props.album.cover) cover = {backgroundImage: 'url("' + this.state.cover + '")'}
     return (
       <li onClick={this.handleClick} onMouseOver={this.activate} onMouseOut={this.reset} style={[styles.base, store.getState().library.newest ? {order: (this.props.album.time * -1)} : '' ]}>
-        <div style={[styles.cover, cover, this.state.active ? styles.zoom : '']}></div>
+        <div style={this.state.fade ? styles.fade : styles.nonfade}>
+          <div ref='album' style={[styles.cover, cover, this.state.active ? styles.zoom : '']}></div>
+        </div>
         <span style={[this.state.active ? styles.active : '']}>{this.props.album.artist + ' - ' + this.props.album.title}</span>
       </li>
     )
@@ -46,7 +66,7 @@ const styles = {
     backgroundPosition: 'center center',
     backgroundColor: '#2b2b2b',
     paddingTop: '100%',
-    transition: 'transform .25s',
+    transition: 'transform .25s, opacity .25s',
     border: '2px solid #181818',
     marginBottom: '.5em',
     transform: 'scale(0.98)'
@@ -57,5 +77,13 @@ const styles = {
   zoom: {
     transform: 'scale(1)',
     border: '2px solid rgba(92, 67, 232, .8)'
+  },
+  nonfade: {
+    transition: '.5s',
+    transitionDelay: '.25s'
+  },
+  fade: {
+    opacity: 0,
+    transform: 'scale(.9)'
   }
 }
