3d418a14fc0570c174bb213831b6ef16c519b207
mashaal <omar@teacups.io>
2016-09-17 19:31:45 +1000

    menu bar animation
    Signed-off-by: mashaal <omar@teacups.io>

--- components/bar/index.jsx
+++ components/bar/index.jsx
@@ -13,12 +13,15 @@ export default class Bar extends Component {
 const styles = {
   base: {
     width: '100vw',
-    height: '1.75em',
+    height: '1.5em',
     position: 'fixed',
     top: 0,
     left: 0,
     zIndex: 22,
     webkitAppRegion: 'drag',
-    backgroundColor: 'rgba(0, 0, 0, .125)'
+    transition: '.5s',
+    ':hover': {
+      backgroundColor: 'rgba(92, 67, 232, .8)'
+    }
   }
 }
