11682d881dd47e2672bf77b856fb4e3134201577
mashaal <omar@teacups.io>
2017-04-02 11:02:52 +1100

    touchbar

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const {app, BrowserWindow, TouchBar} = require('electron')
+const {TouchBarLabel, TouchBarSpacer} = TouchBar
 const windowStateKeeper = require('electron-window-state')
 let mainWindow
-let player
-const spawn = require('child_process').spawn
-const ffmpeg = require('fluent-ffmpeg')
-const fs = require('fs')
-const userData = app.getPath('userData')
-var extract = require('extract-zip')
+
+const artist = new TouchBarLabel({textColor: '#5c43e8'})
+const track = new TouchBarLabel({textColor: '#555555'})
+const time = new TouchBarLabel({textColor: '#5c43e8'})
+
+const touchBar = new TouchBar([
+  artist,
+  new TouchBarSpacer({size: 'small'}),
+  track,
+  new TouchBarSpacer({size: 'small'}),
+  time
+])
 
 const createWindow = () => {
   let mainWindowState = windowStateKeeper({
@@ -28,9 +33,9 @@ const createWindow = () => {
     }
   })
   mainWindowState.manage(mainWindow)
-  mainWindow.loadURL('file://' + __dirname + '/components/app/index.html')
+  mainWindow.loadURL(`file://${__dirname}/components/app/index.html`)
+  mainWindow.setTouchBar(touchBar)
   mainWindow.on('closed', () => {
-    app.stop()
     mainWindow = null
   })
 }
@@ -39,7 +44,6 @@ app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.stop()
     app.quit()
   }
 })
@@ -50,57 +54,11 @@ app.on('activate', () => {
   }
 })
 
-app.playTrack = file => {
-  app.stop()
-  player = spawn(`${userData}/ffmpeg/ffplay`, [file.path, '-nodisp', '-autoexit'], {stdio: 'ignore'})
-  ffmpeg.setFfprobePath(`${userData}/ffmpeg/ffprobe`)
-  ffmpeg.ffprobe(file.path, (error, metadata) => {
-    let duration
-    if (error) throw error
-    else if (metadata.format) {
-      duration = metadata.format.duration
-    } else duration = false
-    mainWindow.webContents.executeJavaScript(`window.player.setDuration(${duration})`)
-  })
-  player.on('exit', sss)
-  this.playing = this.cnt = true
-  this.track = file
+app.updateTouchBar = metadata => {
+  artist.label = metadata.artist
+  track.label = metadata.track
 }
 
-app.currentTrack = () => this.track
-
-const sss = () => {
-  if (this.cnt) next()
-  else app.stop()
-}
-
-let next = () => mainWindow.webContents.executeJavaScript('window.player.next()')
-
-app.stop = () => {
-  if (player) player.removeListener('exit', sss)
-  if (this.playing) player.kill()
-  player = this.playing = this.cnt = this.track = null
-}
-
-app.pause = () => {
-  this.playing = false
-  mainWindow.webContents.executeJavaScript('window.player.pause()')
-  player.kill('SIGSTOP')
-}
-
-app.resume = () => {
-  this.playing = true
-  mainWindow.webContents.executeJavaScript('window.player.resume()')
-  player.kill('SIGCONT')
-}
-
-app.toggle = () => {
-  if (this.playing) app.pause()
-  else app.resume()
-}
-
-if (!fs.existsSync(`${userData}/ff.txt`)) {
-  extract(__dirname + '/ffmpeg.zip', {dir: userData}, function (err) {
-   fs.writeFile(`${userData}/ff.txt`, '🍑🍆')
-  })
+app.updateTouchBarTime = duration => {
+  time.label = duration
 }
